fc2549785a0b8b9608da40e4a0efdf49c97c4e07
Guilherme Sehn <ghsehn@gmail.com>
2015-10-16 17:03:03 -0300

    Added authorization feature

--- lib/rails_db.rb
+++ lib/rails_db.rb
@@ -6,10 +6,18 @@ require "jquery-rails"
 require 'codemirror-rails'
 
 module RailsDb
+  mattr_accessor :authorization
+  @@authorization = proc { true }
+
   # Custom require relative that work with older rubies also
   def self.require_relative(path)
     full_path = File.expand_path(path, File.dirname(__FILE__))
     Kernel.require(full_path)
+  end
+
+  # Allows the user to specify a current authorization method
+  def self.require_authorization(&block)
+    @@authorization = block
   end
 end
 
