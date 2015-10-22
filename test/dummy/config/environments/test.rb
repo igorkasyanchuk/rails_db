2f0e88aacb427af393eed8dd8b62c7c1df0b3040
Igor Kasyanchuk <igorkasyanchuk@gmail.com>
2015-10-22 14:54:33 +0300

    added secrets

--- test/dummy/config/environments/test.rb
+++ test/dummy/config/environments/test.rb
@@ -5,6 +5,9 @@ Dummy::Application.configure do
   # test suite. You never need to work with it otherwise. Remember that
   # your test database is "scratch space" for the test suite and is wiped
   # and recreated between test runs. Don't rely on the data there!
+  config.secret_key_base = ('x'*30) rescue nil
+  config.secret_token = ('x'*30) rescue nil
+
   config.cache_classes = true
 
   # Do not eager load code on boot. This avoids loading your whole application
