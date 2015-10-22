a1f18216115b3e020059d53bb89169fb42b56dda
Igor Kasyanchuk <igorkasyanchuk@gmail.com>
2015-10-22 09:42:47 +0300

    working on different DB

---
+++ test/dummy/app/models/populate.rb
@@ -0,0 +1,38 @@
+class Populate
+
+  def self.populate
+    require 'populator'
+    require 'faker'
+
+    Account.populate 20 do |account|
+      account.name = Faker::Company.name
+      account.users_count = rand(100)
+    end
+
+    Comment.populate 500 do |comment|
+      comment.user_id = rand(100)
+      comment.comment = Populator.sentences(2..10)
+    end
+
+    Contact.populate 200 do |contact|
+      contact.name =  Faker::Name.name
+      contact.email = Faker::Internet.email
+      contact.message = Populator.sentences(2..10)
+    end
+
+    User.populate 100 do |user|
+      user.name = Faker::Name.name
+      user.age = rand(99)
+      user.salary = rand(5000)
+      user.bio = Populator.sentences(2..10)
+      user.account_id = rand(3)
+      user.dob = Date.today - rand(10000).days
+    end
+
+    Project.populate 100 do |project|
+      project.name = Faker::Company.name
+    end
+  end
+
+end
+
