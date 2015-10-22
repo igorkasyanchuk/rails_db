af081ca1e31075099d392c808f348aa4a4ddced9
Igor Kasyanchuk <igorkasyanchuk@gmail.com>
2015-10-22 10:02:41 +0300

    logging for DB

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,14 @@
 # Configure Rails Environment
 ENV["RAILS_ENV"] = "test"
 
+puts "TRYING TO USE DB: #{ENV['DB'] || 'default'}"
+
 require File.expand_path("../../test/dummy/config/environment.rb",  __FILE__)
 ActiveRecord::Migrator.migrations_paths = [File.expand_path("../../test/dummy/db/migrate", __FILE__)]
 ActiveRecord::Migrator.migrations_paths << File.expand_path('../../db/migrate', __FILE__)
 require "rails/test_help"
+
+puts "USING DB: #{RailsDb::Database.adapter.adapter_name}"
 
 # Filter out Minitest backtrace while allowing backtrace from other libraries
 # to be shown.
@@ -12,13 +16,6 @@ Minitest.backtrace_filter = Minitest::BacktraceFilter.new
 
 # Load support files
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
-
-# Load fixtures from the engine
-if ActiveSupport::TestCase.respond_to?(:fixture_path=)
-  ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
-  ActionDispatch::IntegrationTest.fixture_path = ActiveSupport::TestCase.fixture_path
-  ActiveSupport::TestCase.fixtures :all
-end
 
 def clean_db
   ActiveRecord::Base.connection.tables.map(&:classify).map{|name| name.constantize if Object.const_defined?(name)}.compact.each(&:delete_all)
