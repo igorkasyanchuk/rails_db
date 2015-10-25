cd1b6cd0596be02d94870e116ccaf500a39d46b2
Igor Kasyanchuk <igorkasyanchuk@gmail.com>
2015-10-25 19:14:18 +0200

    fixing tests

--- test/rails_db_data_table_helper_test.rb
+++ test/rails_db_data_table_helper_test.rb
@@ -11,21 +11,21 @@ class RailsDbDataTableHelperTest < ActionView::TestCase
     user = User.create(name: 'Igor')
     content = rails_db_data_table(table: 'users')
     assert_match 'Igor', content
-    assert_no_match 'BOB MARLEY', content
+    assert_no_match /BOB MARLEY/, content
   end
 
   test "default rails_db_data_table_sql works" do
     user = User.create(name: 'Igor')
     content = rails_db_data_table_sql(sql: 'select * from users')
     assert_match 'Igor', content
-    assert_no_match 'BOB MARLEY', content
+    assert_no_match /BOB MARLEY/, content
   end
 
   test "rails_db_data_table works with many options" do
     user = User.create(name: 'Igor', age: 9999)
     content = rails_db_data_table(table: 'users', limit: 10, order_by: :id, order: :asc, header: false, footer: true, style: :bootstrap, columns: [:id, :name])
     assert_match 'Igor', content
-    assert_no_match 'BOB MARLEY', content
+    assert_no_match /BOB MARLEY/, content
     assert_no_match '9999', content
   end
 
@@ -33,7 +33,7 @@ class RailsDbDataTableHelperTest < ActionView::TestCase
     user = User.create(name: 'Igor')
     content = rails_db_data_table_sql(sql: 'select * from users', header: false, footer: true, style: :bootstrap)
     assert_match 'Igor', content
-    assert_no_match 'BOB MARLEY', content
+    assert_no_match /BOB MARLEY/, content
   end
 
 end
