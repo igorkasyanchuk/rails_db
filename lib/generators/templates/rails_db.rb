720f0d71658573b2ede46a7a7b04e123c6b2d2b0
Igor Kasyanchuk <igorkasyanchuk@gmail.com>
2015-10-26 22:36:05 +0200

    If defined RailsDb in initializer added

--- lib/generators/templates/rails_db.rb
+++ lib/generators/templates/rails_db.rb
@@ -1,25 +1,27 @@
-RailsDb.setup do |config|
-  # # enabled or not
-  # config.enabled = Rails.env.to_s == 'development'
+if Object.const_defined?('RailsDb')
+  RailsDb.setup do |config|
+    # # enabled or not
+    # config.enabled = Rails.env.to_s == 'development'
 
-  # # automatic engine routes mounting
-  # config.automatic_routes_mount = true
+    # # automatic engine routes mounting
+    # config.automatic_routes_mount = true
 
-  # set tables which you want to hide ONLY
-  # config.black_list_tables = ['users', 'accounts']
+    # set tables which you want to hide ONLY
+    # config.black_list_tables = ['users', 'accounts']
 
-  # set tables which you want to show ONLY
-  # config.white_list_tables = ['posts', 'comments']
+    # set tables which you want to show ONLY
+    # config.white_list_tables = ['posts', 'comments']
 
-  # # Enable http basic authentication
-  # config.http_basic_authentication_enabled = false
+    # # Enable http basic authentication
+    # config.http_basic_authentication_enabled = false
 
-  # # Enable http basic authentication
-  # config.http_basic_authentication_user_name = 'rails_db'
+    # # Enable http basic authentication
+    # config.http_basic_authentication_user_name = 'rails_db'
 
-  # # Enable http basic authentication
-  # config.http_basic_authentication_password = 'password'
+    # # Enable http basic authentication
+    # config.http_basic_authentication_password = 'password'
 
-  # # Enable http basic authentication
-  # config.verify_access_proc = proc { |controller| true }
+    # # Enable http basic authentication
+    # config.verify_access_proc = proc { |controller| true }
+  end
 end
