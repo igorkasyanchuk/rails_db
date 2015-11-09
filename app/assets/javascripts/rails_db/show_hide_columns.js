80cf3b3cf41d069278354c637550be152c964380
Andriy Samokhin <asam@softserveinc.com>
2015-11-09 01:50:42 -0800

    Refactored JavaScript which shows and hides columns

--- app/assets/javascripts/rails_db/show_hide_columns.js
+++ app/assets/javascripts/rails_db/show_hide_columns.js
@@ -5,24 +5,13 @@ $(function(){
     $('.rails-db-settings').toggle();
   });
 
-  $('body').on('click', '.column-selection input, .column-name', function() {
+  $('body').on('click', '.column-name input', function() {
     table_name = $('h2')[0].innerHTML;
-    if($(this).hasClass('column-name')){
-      column_name = $(this)[0].innerHTML.replace(/\s+/, '').replace(/\s+/, '');
-      if($(this).prev().prop('checked') == false){
-        $(this).prev().prop('checked', true)
-        remove_column_from_cookie(table_name, column_name);
-      } else {
-        $(this).prev().prop('checked', false)
-        write_column_to_cookie(table_name, column_name);
-      }
+    column_name = $(this).prop('name');
+    if($(this).prop('checked') == false){
+      write_column_to_cookie(table_name, column_name);
     } else {
-      column_name = $(this).prop('name');
-      if($(this).prop('checked') == false){
-        write_column_to_cookie(table_name, column_name);
-      } else {
-        remove_column_from_cookie(table_name, column_name);
-      }
+      remove_column_from_cookie(table_name, column_name);
     }
     $('th.column_' + column_name).toggle();
     $('td.column_' + column_name).toggle();
