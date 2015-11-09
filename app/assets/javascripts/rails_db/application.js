2ac3d2ff61b41d02138c2329d58f108c23ec0da9
Andriy Samokhin <asam@softserveinc.com>
2015-11-09 00:47:38 -0800

    Removed code which shows and hides columns from 'application.js'

--- app/assets/javascripts/rails_db/application.js
+++ app/assets/javascripts/rails_db/application.js
@@ -8,6 +8,7 @@
 //= require codemirror/modes/sql
 //= require codemirror/addons/hint/show-hint.js
 //= require codemirror/addons/hint/sql-hint.js
+//= require rails_db/show_hide_columns.js
 
 $(function(){
   $(document).foundation();
@@ -33,34 +34,6 @@ $(function(){
     save_expand_collapse();
     return false;
   });
-
-  $('body').on('click', '.settings-link', function() {
-    $('.rails-db-settings').toggle();
-  });
-
-  $('body').on('click', '.column-selection input, .column-name', function() {
-    table_name = $('h2')[0].innerHTML;
-    if($(this).hasClass('column-name')){
-      column_name = $(this)[0].innerHTML.replace(/\s+/, '').replace(/\s+/, '');
-      if($(this).prev().prop('checked') == false){
-        $(this).prev().prop('checked', true)
-        remove_column_from_cookie(table_name, column_name);
-      } else {
-        $(this).prev().prop('checked', false)
-        write_column_to_cookie(table_name, column_name);
-      }
-    } else {
-      column_name = $(this).prop('name');
-      if($(this).prop('checked') == false){
-        write_column_to_cookie(table_name, column_name);
-      } else {
-        remove_column_from_cookie(table_name, column_name);
-      }
-    }
-    $('th.column_' + column_name).toggle();
-    $('td.column_' + column_name).toggle();
-  });
-
 });
 
 function expand_collapse() {
@@ -75,24 +48,6 @@ function expand_collapse() {
 function save_expand_collapse() {
   $.removeCookie('sidebar_visible');
   $.cookie('sidebar_visible', $('.expand').is(':visible'), { expires: 30, path: '/' });
-};
-
-function write_column_to_cookie(table_name, column_name) {
-  column_names = get_column_names_from_cookie(table_name);
-  $.cookie(table_name, column_names += ', ' + column_name);
-};
-
-function remove_column_from_cookie(table_name, column_name) {
-  column_names = get_column_names_from_cookie(table_name);
-  $.cookie(table_name, column_names.replace(', ' + column_name, ''));
-};
-
-function get_column_names_from_cookie(table_name) {
-  if($.cookie(table_name) == undefined) {
-    return '';
-  } else {
-    return $.cookie(table_name);
-  }
 };
 
 // https://github.com/codemirror/CodeMirror/blob/master/mode/sql/index.html
