a87a3280838f5cd6e20f37b0ce96f76d737f14cb
Andriy Samokhin <asam@softserveinc.com>
2015-11-10 04:09:12 -0800

    Tests for 'checked' helper

---
+++ test/tables_helper_test.rb
@@ -0,0 +1,30 @@
+require 'test_helper'
+
+class TablesHelperTest < ActionView::TestCase
+  include RailsDb::TablesHelper
+
+  def setup
+    clean_db
+  end
+
+  test "method checked returns true for checkbox status if cookie does not contain hash_key for this table" do
+    params[:table_id] = 'users'
+    status = checked('created_at')
+    assert_equal true, status
+  end
+
+  test "method checked returns true for checkbox status if cookie does not contain record for this table:column" do
+    params[:table_id] = 'users'
+    cookies['Table: users'] = 'created_at'
+    status = checked('name')
+    assert_equal true, status
+  end
+
+  test "method checked returns false for checkbox status if cookie contains record for this table:column" do
+    params[:table_id] = 'users'
+    cookies['Table: users'] = 'created_at'
+    status = checked('created_at')
+    assert_equal false, status
+  end
+
+end
