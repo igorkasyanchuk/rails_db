d5a2a7372d9c24e1a7e5bce57633c314e897d323
Andriy Samokhin <asam@softserveinc.com>
2015-11-10 04:46:41 -0800

    Renamed helper method into rspec tests

--- test/tables_helper_test.rb
+++ test/tables_helper_test.rb
@@ -9,21 +9,21 @@ class TablesHelperTest < ActionView::TestCase
 
   test "method checked returns true for checkbox status if cookie does not contain hash_key for this table" do
     params[:table_id] = 'users'
-    status = checked('created_at')
+    status = column_is_checked?('created_at')
     assert_equal true, status
   end
 
   test "method checked returns true for checkbox status if cookie does not contain record for this table:column" do
     params[:table_id] = 'users'
     cookies['Table: users'] = 'created_at'
-    status = checked('name')
+    status = column_is_checked?('name')
     assert_equal true, status
   end
 
   test "method checked returns false for checkbox status if cookie contains record for this table:column" do
     params[:table_id] = 'users'
     cookies['Table: users'] = 'created_at'
-    status = checked('created_at')
+    status = column_is_checked?('created_at')
     assert_equal false, status
   end
 
