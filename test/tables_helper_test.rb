b08ebb902087ad7ba2008d3aa3b2b396f67444a1
Andriy Samokhin <asam@softserveinc.com>
2015-11-11 03:21:11 -0800

    Refactored rspec tests

--- test/tables_helper_test.rb
+++ test/tables_helper_test.rb
@@ -13,27 +13,27 @@ class TablesHelperTest < ActionView::TestCase
   end
 
   test "method column_is_checked? returns true for checkbox status if cookie does not contain record for this table:column" do
-    cookies['Table: users'] = ' created_at,'
+    cookies['Table: users'] = 'created_at,'
     status = column_is_checked?('users', 'name')
     assert_equal true, status
   end
 
   test "method column_is_checked? returns false for checkbox status if cookie contains record for this table:column" do
-    cookies['Table: users'] = ' created_at,'
+    cookies['Table: users'] = 'created_at,'
     status = column_is_checked?('users', 'created_at')
     assert_equal false, status
   end
 
   test "method display_style_column returns 'display:none' if cookie contains record for this table:column" do
-    cookies['Table: users'] = ' created_at,'
+    cookies['Table: users'] = 'created_at,'
     style = display_style_column('users', 'created_at')
-    assert_match 'display:none', style
+    assert_equal 'display:none', style
   end
 
   test "method display_style_column returns 'display' if cookie does not contain record for this table:column" do
-    cookies['Table: users'] = ' created_at,'
+    cookies['Table: users'] = 'created_at,'
     style = display_style_column('users', 'name')
-    assert_match 'display', style
+    assert_equal 'display', style
   end
 
 end
