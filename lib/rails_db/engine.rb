9c49df6eab0a104f7ab9caeacf1b49a1288c7469
Igor Kasyanchuk <igorkasyanchuk@gmail.com>
2015-12-31 21:48:25 +0200

    added logo to precompile

--- lib/rails_db/engine.rb
+++ lib/rails_db/engine.rb
@@ -4,7 +4,7 @@ module RailsDb
     config.autoload_paths += Dir["#{config.root}/lib"]
 
     initializer 'any_login.assets_precompile', :group => :all do |app|
-      app.config.assets.precompile += ['vendor/modernizr.js', 'rails_db/up_arrow.gif', 'rails_db/down_arrow.gif']
+      app.config.assets.precompile += ['vendor/modernizr.js', 'rails_db/up_arrow.gif', 'rails_db/down_arrow.gif', 'rails_db/logo.png']
       app.config.assets.precompile += ["codemirror*", "codemirror/**/*"]
     end
 
