4b8d284f2429cf1f09d95d0295f0d9a580bbf048
kyuden <msmsms.um@gmail.com>
2016-06-03 02:34:43 +0900

    Use env helper

--- lib/generators/templates/rails_db.rb
+++ lib/generators/templates/rails_db.rb
@@ -1,7 +1,7 @@
 if Object.const_defined?('RailsDb')
   RailsDb.setup do |config|
     # # enabled or not
-    # config.enabled = Rails.env.to_s == 'development'
+    # config.enabled = Rails.env.development?
 
     # # automatic engine routes mounting
     # config.automatic_routes_mount = true
