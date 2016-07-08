9ec83c4fb8190ff3c2c2e8159a95ce05e6f467cd
Igor Kasyanchuk <igorkasyanchuk@gmail.com>
2016-07-08 08:06:28 +0300

    only for Rails 5

--- lib/ext/ransack_ext.rb
+++ lib/ext/ransack_ext.rb
@@ -1,22 +1,26 @@
-module Ransack
-  module Helpers
-    module FormHelper
+if Rails::VERSION::MAJOR >= 5
 
-      class SortLink
+  module Ransack
+    module Helpers
+      module FormHelper
 
-        def url_options_with_feature
-          result = url_options_without_feature
-          if result.respond_to?(:permit!)
-            result.permit!
-          else
-            result
+        class SortLink
+
+          def url_options_with_feature
+            result = url_options_without_feature
+            if result.respond_to?(:permit!)
+              result.permit!
+            else
+              result
+            end
           end
+
+          alias_method :url_options_without_feature, :url_options
+          alias_method :url_options, :url_options_with_feature
         end
 
-        alias_method :url_options_without_feature, :url_options
-        alias_method :url_options, :url_options_with_feature
       end
-
     end
   end
+
 end
