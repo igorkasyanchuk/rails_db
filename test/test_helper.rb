e4b4c5dc12c19712143cd9b14a5c5e7ae1bf7e72
Igor Kasyanchuk <igorkasyanchuk@gmail.com>
2018-05-12 15:30:15 -0700

    cleaning code

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,11 +8,6 @@ ActiveRecord::Migrator.migrations_paths = [File.expand_path("../../test/dummy/db
 ActiveRecord::Migrator.migrations_paths << File.expand_path('../../db/migrate', __FILE__)
 require "rails/test_help"
 
-begin
-  ActiveSupport::TestCase.use_transactional_fixtures = false
-rescue NoMethodError
-end
-
 puts "USING DB: #{RailsDb::Database.adapter.adapter_name}"
 puts "USING Rails: #{Gem.loaded_specs['rails'].version}"
 
@@ -24,8 +19,10 @@ Minitest.backtrace_filter = Minitest::BacktraceFilter.new
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
 def clean_db
-  if ActiveRecord::Base.connection.table_exists? :t
-    ActiveRecord::Base.connection.drop_table :t
-  end
-  ActiveRecord::Base.connection.tables.map(&:classify).map{|name| name.constantize if Object.const_defined?(name)}.compact.each(&:delete_all)
+  # if ActiveRecord::Base.connection.table_exists? :t
+  #   ActiveRecord::Base.connection.drop_table :t
+  # end
+  # ActiveRecord::Base.connection.tables.map(&:classify).map{|name| name.constantize if Object.const_defined?(name)}.compact.each(&:delete_all)
 end
+
+clean_db
