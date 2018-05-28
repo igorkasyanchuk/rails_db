27ca0048b2b43e5a0124788a9507518f2a58cc3d
Igor Kasyanchuk <igorkasyanchuk@gmail.com>
2018-05-28 10:10:11 -0400

    use older format of rails migration

--- test/dummy/db/migrate/20180528134358_add_deleted_at_to_projects.rb
+++ test/dummy/db/migrate/20180528134358_add_deleted_at_to_projects.rb
@@ -1,4 +1,4 @@
-class AddDeletedAtToProjects < ActiveRecord::Migration[5.1]
+class AddDeletedAtToProjects < ActiveRecord::Migration[4.2]
   def change
     add_column :projects, :deleted_at, :datetime
     add_index :projects, :deleted_at
