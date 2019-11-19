f589e289eebd201684d84fa21054fd7507d137b5
Igor Kasyanchuk <igorkasyanchuk@gmail.com>
2019-11-19 11:02:34 -0500

    reduce size of gem file

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 rm *.gem
 rm -fr test/dummy/log/*.log
-rm -fr test/standalone/log/*.log
 rm -fr test/dummy/tmp/cache
+rm -fr test/dummy/tmp/storage
+rm -fr test/standalone/log/*.log
 rm -fr test/standalone/tmp/cache
+rm -fr test/standalone/tmp/storage
 gem build rails_db.gemspec
