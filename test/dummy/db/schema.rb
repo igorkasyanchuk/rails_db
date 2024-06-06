cd9bbbfc123e0549de298c3500b729ba483a43af
Igor Kasyanchuk <igorkasyanchuk@gmail.com>
2024-06-06 13:58:28 +0300

    ransack fixes

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -11,21 +11,21 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2021_06_26_083314) do
-  create_table "accounts", id: :integer, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "accounts", force: :cascade do |t|
     t.string "name"
     t.integer "users_count"
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
   end
 
-  create_table "comments", id: :integer, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "comments", force: :cascade do |t|
     t.text "comment"
     t.integer "user_id"
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
   end
 
-  create_table "contacts", id: :integer, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "contacts", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.text "message"
@@ -34,77 +34,77 @@ ActiveRecord::Schema[7.1].define(version: 2021_06_26_083314) do
     t.index ["name", "email"], name: "index_contacts_on_name_and_email"
   end
 
-  create_table "legacy_accounts", primary_key: "uuid", id: :integer, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "legacy_accounts", primary_key: "uuid", force: :cascade do |t|
     t.string "name"
   end
 
-  create_table "payments", id: :integer, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.decimal "amount", precision: 10
+  create_table "payments", force: :cascade do |t|
+    t.decimal "amount"
     t.integer "project_id"
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
     t.index ["project_id"], name: "index_payments_on_project_id"
   end
 
-  create_table "projects", id: :integer, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "projects", force: :cascade do |t|
     t.string "name"
-    t.string "description_524218"
-    t.string "description_582477"
-    t.string "description_910553"
-    t.string "description_181360"
-    t.string "description_637048"
-    t.string "description_920060"
-    t.string "description_125376"
-    t.string "description_72387"
-    t.string "description_457825"
-    t.string "description_858192"
-    t.string "description_292867"
-    t.string "description_986009"
-    t.string "description_293297"
-    t.string "description_733485"
-    t.string "description_248137"
-    t.string "description_273482"
-    t.string "description_520504"
-    t.string "description_785302"
-    t.string "description_224435"
-    t.string "description_235085"
-    t.integer "amount_97144"
-    t.integer "amount_116203"
-    t.integer "amount_394760"
-    t.integer "amount_542072"
-    t.integer "amount_986307"
-    t.integer "amount_571123"
-    t.integer "amount_344510"
-    t.integer "amount_434757"
-    t.integer "amount_728495"
-    t.integer "amount_878167"
-    t.integer "amount_518288"
-    t.integer "amount_767100"
-    t.integer "amount_119368"
-    t.integer "amount_793397"
-    t.integer "amount_206014"
-    t.integer "amount_693084"
-    t.integer "amount_230674"
-    t.integer "amount_341979"
-    t.integer "amount_466015"
-    t.integer "amount_128321"
+    t.string "description_214636"
+    t.string "description_881927"
+    t.string "description_377910"
+    t.string "description_787561"
+    t.string "description_397939"
+    t.string "description_427609"
+    t.string "description_70125"
+    t.string "description_68088"
+    t.string "description_211155"
+    t.string "description_915979"
+    t.string "description_198834"
+    t.string "description_669085"
+    t.string "description_771"
+    t.string "description_519606"
+    t.string "description_500107"
+    t.string "description_277668"
+    t.string "description_706655"
+    t.string "description_617984"
+    t.string "description_312370"
+    t.string "description_239866"
+    t.integer "amount_777579"
+    t.integer "amount_886090"
+    t.integer "amount_11955"
+    t.integer "amount_494653"
+    t.integer "amount_666834"
+    t.integer "amount_297923"
+    t.integer "amount_458470"
+    t.integer "amount_339355"
+    t.integer "amount_539001"
+    t.integer "amount_209067"
+    t.integer "amount_74088"
+    t.integer "amount_736364"
+    t.integer "amount_685429"
+    t.integer "amount_506310"
+    t.integer "amount_440103"
+    t.integer "amount_418280"
+    t.integer "amount_60289"
+    t.integer "amount_433505"
+    t.integer "amount_160749"
+    t.integer "amount_723751"
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
     t.datetime "deleted_at", precision: nil
     t.index ["deleted_at"], name: "index_projects_on_deleted_at"
   end
 
-  create_table "projects_users", id: false, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "projects_users", id: false, force: :cascade do |t|
     t.integer "project_id"
     t.integer "user_id"
     t.index ["project_id"], name: "index_projects_users_on_project_id"
     t.index ["user_id"], name: "index_projects_users_on_user_id"
   end
 
-  create_table "users", id: :integer, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "name"
     t.integer "age"
-    t.decimal "salary", precision: 10
+    t.decimal "salary"
     t.text "bio"
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
