9ff1e120ee3c8df738d4f8c5828ccaf2caef9b6c
Ben Purinton <ben.purinton@gmail.com>
2025-07-07 14:44:32 -0700

    Fix backwards compatibility

--- lib/rails_db/engine.rb
+++ lib/rails_db/engine.rb
@@ -7,8 +7,30 @@ module RailsDb
       # Only configure asset precompilation if Sprockets is available
       if defined?(Sprockets) && app.config.respond_to?(:assets)
         app.config.assets.precompile += [
+          # Original application files
           'rails_db/application.js', 
           'rails_db/application.css',
+          
+          # Individual CSS files for direct inclusion
+          'rails_db/foundation.min.css',
+          'rails_db/foundation_and_overrides.css',
+          'rails_db/app.css',
+          'rails_db/sidebar.css',
+          'rails_db/pagination.css',
+          'rails_db/table.css',
+          'rails_db/forms.css',
+          'rails_db/search.css',
+          
+          # Individual JS files for direct inclusion
+          'rails_db/jquery.js',
+          'rails_db/jquery.cookie.js',
+          'rails_db/rails-ujs.js',
+          'rails_db/foundation.min.js',
+          'rails_db/sticky.js',
+          'rails_db/show_hide_columns.js',
+          'rails_db/search.js',
+          
+          # Images
           'rails_db/logo.png',
           'rails_db/logo_mini.png',
           'rails_db/data_table_code.png',
@@ -16,6 +38,8 @@ module RailsDb
           'rails_db/railsdb.png',
           'rails_db/runsql.png',
           'rails_db/*.png', 
+          
+          # CodeMirror assets
           'codemirror/*', 
           'codemirror/**/*'
         ]
